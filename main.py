18b502e5ca18c5ba3f8797f32ec23222e6fa99f5
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-19 10:36:24 -0400

    Added AD quotes and bigram poetry method

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print('hello world')
+import random
+import bigram
+
+with open('arrested_development.txt') as file:
+    ad = file.read()
+
+ad = ad.split(',')
+phrase = random.choice(ad)
+
+print(bigram.bigram_poem(phrase))
 

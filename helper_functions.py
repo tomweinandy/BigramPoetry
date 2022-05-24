d43afa48562089105b7a1f44a8ed407ce56b7804
Thomas Weinandy <tomweinandy@gmail.com>
2022-05-24 11:48:45 -0400

    Queries Twitter and creates a bigram poem

--- helper_functions.py
+++ helper_functions.py
@@ -1,14 +1,48 @@
-
-
-def true(tweet):
-    condition = 'RT' not in tweet.text
-    return condition
+import nltk
+import datetime as dt
 
 
 def conditions_met(tweet):
     conditions_list = ['RT' not in tweet.text,
                        len(tweet.text.split(' ')) > 2,
-                       'http' not in tweet.text,
+                       # 'http' not in tweet.text,
                        tweet.lang == 'en',
                        tweet.user.screen_name != 'BrigramPoetry']
     return all(conditions_list)
+
+
+def cleaner(phrase):
+    # phrase = phrase.replace('\n', ' ').replace('VIDEO', '')  # remove edge cases with bad formatting
+    # phrase = phrase.replace(' -', ' ').replace(' –', ' ').replace(' ‘', '')
+    # phrase = phrase.replace('- ', ' ').replace(' /', ' ').replace('— ', ' ')
+    # phrase = phrase.replace(': ', ' ').replace(':)', '').replace('&amp', 'and')
+
+    rejects = '¿.?!,[]|"“();…{}«•*+@~<>'                                  # define punctuation to be removed
+    phrase_reject = phrase.translate({ord(c): None for c in rejects})     # remove defined punctuation
+    phrase_split = phrase_reject.split(' ')                               # split phrase by whitespace
+    phrase_clear = list(filter(None, phrase_split))                       # strip any extra whitespace
+    return phrase_clear
+
+
+def bigram_poem(phrase_raw):                                              # define new function bigram_poem
+    phrase_cleaned = cleaner(phrase_raw)
+    phrase_shortened = phrase_cleaned[0:8]                                # only use the first 8 terms
+    phrase_bigram = list(nltk.bigrams(phrase_shortened))                  # convert to bigram list
+    poem = ''                                                             # create new string
+    for bigram in phrase_bigram:                                          # loop through bigrams
+        poem += ' ' + bigram[0] + ' ' + bigram[1] + ' \n'                 # add looped bigrams to string w line break
+    return poem
+
+
+def bigram_tweet(tweet):
+    # Compile tweet
+    header = f'A Bigram Poem inspired by {tweet.user.screen_name}:\n'
+    # print(header)
+    poem = bigram_poem(tweet.text)
+    footer = f'   - {tweet.user.name}'
+    full_tweet = header + poem + footer
+
+    # twitter.update_status(status=full_tweet)  # tweet out on @BigramPoetry account
+
+    print(full_tweet)  # print result, timestamp (for testing)
+    print(dt.datetime.now(), '\n')
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import random
-import bigram
+import helper_functions
 
 with open('arrested_development.txt') as file:
     ad = file.read()
 
 ad = ad.split(',')
 phrase = random.choice(ad)
+poem = helper_functions.bigram_poem(phrase)
 
-print(bigram.bigram_poem(phrase))
+print(poem)
+
+
+
+# Load credentials
+import tweepy
+import json
+
+with open('../../Dropbox/100DaysOfCodePRIVATE/JTCreds.json') as file:
+    creds = json.loads(file.read())
+
+API_KEY = creds['API_KEY']
+API_SECRET = creds['API_SECRET']
+ACCESS_TOKEN = creds['ACCESS_TOKEN']
+ACCESS_SECRET = creds['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+# Query
+query = api.search_tweets(q='machine learning', lang='en', count=100)
+
+
+for i in range(len(query)):
+    tweet = query[i]
+
+    if helper_functions.conditions_met(tweet):
+
+        helper_functions.bigram_tweet(tweet)
+
+        break
 
